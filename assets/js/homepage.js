23522df496b96bef34733d36df0d5b2b6eb97ada
J.Cole <ccjohn015@gmail.com>
2022-01-05 12:03:02 -0600

    actual files

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,12 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos").then(function(data) {
+var getUserRepos = function(user) {
+    
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-console.log("outside");
-getUserRepos();
+getUserRepos("microsoft");
